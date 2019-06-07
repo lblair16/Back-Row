a5cfad35b7b9574fd6a4450a8d86fd732d261a3e
William-Bowden <william.j.bowden@aol.com>
2019-06-07 19:14:15 -0400

    start of a possible character controller

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float moveSpeed = 10;
+    public float rotSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float z = Input.GetAxis( "Vertical" ) * moveSpeed;
+        float y = Input.GetAxis( "Horizontal" ) * rotSpeed;
+
+        transform.Translate( 0, 0, z );
+        transform.Rotate( 0, y, 0 );
+    }
+}
